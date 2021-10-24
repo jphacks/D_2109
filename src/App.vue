c35f99198f987a7b4250068e6421c82e22073725
Tatsuya Okazaki <180442029@ccmailg.meijo-u.ac.jp>
2021-10-24 15:03:54 +0900

    feature:スタート画面の実装完了

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <header-menu />
-    <code-viewer :code_list="disp_code" />
+    <start-page v-if="disp_flag.start_page" />
+    <header-menu v-if="disp_flag.header_menu" />
+    <code-viewer v-if="disp_flag.code_viewer" :code_list="disp_code" />
   </div>
 </template>
 
 <script>
+import StartPage from "./components/StartPage.vue";
 import CodeViewer from "./components/CodeViewer.vue";
 import HeaderMenu from "./components/HeaderMenu.vue";
 
 export default {
   name: "App",
   components: {
+    StartPage,
     CodeViewer,
     HeaderMenu,
   },
@@ -21,6 +24,11 @@ export default {
         1: "import math",
         2: "def function ( ) :",
         3: "    return True",
+      },
+      disp_flag: {
+        start_page: true,
+        header_menu: false,
+        code_viewer: false,
       },
     };
   },
