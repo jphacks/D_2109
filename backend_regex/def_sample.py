8c236c6af5d110a2d25f5648b1a3e6626df8ff12
kazu <180441147@ccmailg.meijo-u.ac.jp>
2021-10-24 12:22:48 +0900

    classの整形、関数整形の引数部修正

--- backend_regex/def_sample.py
+++ backend_regex/def_sample.py
@@ -1,3 +1,22 @@
-def  	add_box        	(a  	, b = 	3) 				:
+def  	add_box        	(a  	, b, c = 	3) 				:
 	ab = 2
 	return  x
+
+class     	PermissionMixin   :
+	def __init__(self) -> None:
+		pass
+	pass
+
+class BaseUser	()  :
+	def __init__(self) -> None:
+		pass
+	pass
+
+class User  (	   BaseUser,  PermissionMixin	):
+	name = "aaaa"
+
+	def __init__(self) -> None:
+		super().__init__()
+
+	def getName(self):
+		return self.name
