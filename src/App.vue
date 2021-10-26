35d92e2aa8c700861467dbdc0c27e0ddbe4407ec
Tatsuya Okazaki <180442029@ccmailg.meijo-u.ac.jp>
2021-10-26 18:28:21 +0900

    feature:各ページの接続設定の完了

--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,15 @@
     <direct-code-edit
       v-if="disp_flag.direct_code_edit_page"
       @changePage="ChangePage($event)"
-      @codeGen="CodeGenLoading()"
+      @codeGen="CodeGenLoading($event)"
     />
     <loading v-show="disp_flag.loading_page" />
     <code-gen-complete
       v-if="disp_flag.code_gen_complete_page"
       @changePage="ChangePage($event)"
+      @downloadItem="PythonFileDownload($event)"
+      :input_python="input_python"
+      :output_python="output_python"
     />
     <rule-edit
       v-if="disp_flag.rule_edit_page"
@@ -109,13 +112,20 @@ export default {
       link.click();
     },
     PythonFileSelect(file) {
+      this.ChangePage({ page: "loading_page" });
       let json_file = file.file;
       let reader = new FileReader();
       reader.readAsText(json_file);
-      reader.onload = function () {
-        this.output_python = reader.result;
-        console.log(this.output_python);
-      };
+      reader.addEventListener("load", () => {
+        this.input_python = reader.result;
+        console.log(this.input_python);
+        new Promise(function (resolve) {
+          window.setTimeout(resolve, 1000);
+        }).then(() => {
+          this.ChangePage({ page: "code_gen_complete_page" });
+          console.log(this.input_python);
+        });
+      });
     },
     RuleFileSelect(file) {
       let json_file = file.file;
@@ -148,29 +158,26 @@ export default {
         this.rule_flag = false;
       }
     },
-    CodeGenLoading() {
-      console.log("start");
+    CodeGenLoading(target) {
+      this.input_python = target.code;
       this.ChangePage({ page: "loading_page" });
-      this.sleep(4000).then((result) => {
-        console.log(result);
+      // 1秒間待機後にページ遷移
+      new Promise(function (resolve) {
+        window.setTimeout(resolve, 1000);
+      }).then(() => {
+        console.log(this.input_python);
         this.ChangePage({ page: "code_gen_complete_page" });
       });
       console.log("end");
     },
     RuleGenLoading(target) {
-      this.ChangePage({ page: "loading_page" });
+      this.ChangePage({ page: "rule_make_loading" });
       this.rule = target.rule;
       // 1秒間待機後にページ遷移
       new Promise(function (resolve) {
         window.setTimeout(resolve, 1000);
       }).then(() => {
         this.ChangePage({ page: "rule_gen_complete" });
-      });
-    },
-    // 処理を変えるときはここをAPI用の関数に変更
-    sleep(time) {
-      return new Promise(function (resolve) {
-        window.setTimeout(resolve, time);
       });
     },
   },
