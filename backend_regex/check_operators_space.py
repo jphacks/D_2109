a27b8c2ed55d9dee14c03b9c79ace2781ab5404d
takemi <amk0k2takemi@gmail,com>
2021-10-27 20:44:53 +0900

    * の除去を追加

--- backend_regex/check_operators_space.py
+++ backend_regex/check_operators_space.py
@@ -1,5 +1,8 @@
 # 行ごとに実行 : 指定した演算子
-def check_operators_space(line:str):
-  line = re.sub('([a-zA-Z0-9]*)(<>|<=|>=|is not|not in|-=|==|\+=|!=|=|\+|-|/|%|<|>|and|or|not|in|is)([a-zA-Z0-9]*)','\\1\\2\\3 ',line)
-  line = line.replace("  "," ")
-  return line
+def check_operators_space(line: str):
+    line = re.sub(
+        '([a-zA-Z0-9]*)(<>|<=|>=|is not|not in|-=|==|\\+=|!=|=|\\+|-|\\*|/|%|<|>|and|or|not|in|is)([a-zA-Z0-9]*)',
+        '\\1\\2\\3',
+        line)
+    line = line.replace("  ", " ")
+    return line
