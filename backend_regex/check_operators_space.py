947d550be11d0d27d0daa5b8e49a1fb53e6c3ea5
takemi <amk0k2takemi@gmail,com>
2021-10-27 21:03:02 +0900

    class, def 行を無視して実行

--- backend_regex/check_operators_space.py
+++ backend_regex/check_operators_space.py
@@ -1,8 +1,18 @@
+import re
+
+REJEX_METHOD_NAME = "def([ |\t]+)(\\w+)([ |\t]*)\\((.*)\\)([ |\t]*):"
+REJEX_METHOD_NAME_BACK = "def([ |\t]+)(\\w+)([ |\t]*)\\((.*)\\)([ |\t]*)->([ |\t]*)(\\w+)([ |\t]*):"
+REJEX_CLASS_NAME = "class([ |\t]*)(\\w+)([ |\t]*)(\\((.*)\\))*([ |\t]*):"
+
 # 行ごとに実行 : 指定した演算子
+
+
 def check_operators_space(line: str):
-    line = re.sub(
-        '([a-zA-Z0-9]*)(<>|<=|>=|is not|not in|-=|==|\\+=|!=|=|\\+|-|\\*|/|%|<|>|and|or|not|in|is)([a-zA-Z0-9]*)',
-        '\\1\\2\\3',
-        line)
-    line = line.replace("  ", " ")
+    if (~(re.findall(REJEX_METHOD_NAME_BACK, line) or (re.findall(
+            REJEX_METHOD_NAME_BACK)) or (re.findall(REJEX_CLASS_NAME)))):
+        line = re.sub(
+            '([a-zA-Z0-9]*)(<>|<=|>=|is not|not in|-=|==|\\+=|!=|=|\\+|-|\\*|/|%|<|>|and|or|not|in|is)([a-zA-Z0-9]*)',
+            '\\1\\2\\3',
+            line)
+        line = line.replace("  ", " ")
     return line
