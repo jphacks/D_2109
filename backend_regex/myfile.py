0eb60fdd7f8199f48ff62712801233b7e64241b1
kazu <180441147@ccmailg.meijo-u.ac.jp>
2021-10-29 02:15:49 +0900

    Stackを分離

--- backend_regex/myfile.py
+++ backend_regex/myfile.py
@@ -9,27 +9,51 @@
 a=3
 v=2
 def a(a, b, c=2) -> int:
-  pass
+    pass
+
+
+
+
 def add_box(a, b, c=3):
-  ab=2
-  a=3
-  method=   a(ab,a)
-  def aaaa():
-    return ab
-  return ab
+    ab=2
+    a=3
+    method=      a(ab,a)
+
+
+    def aaaa():
+        return ab
+
+
+    return  ab
+
+
+
+
 class PermissionMixin:
-  def __init__(self) -> None:
-    pass
-  def a(self):
-    pass
+    def __init__(self) -> None:
+        pass
+
+
+    def a(self):
+        pass
+
+
+
+
 class BaseUser():
-  def __init__(self) -> None:
+    def __init__(self) -> None:
+        pass
     pass
-  pass
+
+
+
+
 class User(BaseUser, PermissionMixin):
-  name = 'aaaa'
-  def __init__(self) -> None:
-    super().__init__()
-  # [trim] Warning: 関数名に大文字は含められません.
-  def getName(self):
-    return self.name
+    name = 'aaaa'
+    def __init__(self) -> None:
+        super().__init__()
+
+
+    # [trim] Warning: 関数名に大文字は含められません.
+    def getName(self):
+        return self.name
--- backend_regex/test2.py
+++ backend_regex/test2.py
@@ -42,9 +42,19 @@ def make_args(s_lst):
     args += ', ' + s
   return args
 
+# スタックの定義(by 刀祢)
+class MyStack:
+    def __init__(self):
+        self.stack = []
+    def push(self, item):
+        self.stack.append(item)
+    def pop(self):
+        result = self.stack[-1]  # 末尾の要素を変数に取り出す
+        del self.stack[-1]  # リストから要素を削除する
+        return result  # リスト末尾から取り出したデータを返送する
 
 # スタックの定義
-class MyStack:
+class MyStack_Indent:
     def __init__(self, n):
         self.stack = [0]
     def get_top(self):
@@ -81,8 +91,8 @@ def scan_indent_config(lst, op_indent):
   # タブ文字を' '*INDENT_TAB_NUMに置き換え
   lst_cp = list(map(lambda x: re.sub('\t', ' '*INDENT_TAB_NUM, x), lst_cp))
   
-  stack_indent = MyStack(0)
-  stack = MyStack(0)
+  stack_indent = MyStack_Indent(0)
+  stack = MyStack_Indent(0)
 
   bef = 0
   lst_after = []
@@ -440,8 +450,8 @@ def blank_lines(lst, opt):
 	SENTINEL = 1000000
 
 	### def・classブロックのサーチ ### 
-	stack_blank = MyStack(-1)
-	stack_start_line = MyStack(-1)
+	stack_blank = MyStack()
+	stack_start_line = MyStack()
 
 	line_glob = []
 	line_local = []
