d505abd35174f309929710cd81ce7ded15f2748c
takemi <amk0k2takemi@gmail,com>
2021-11-17 15:45:02 +0900

    check_operaors_space 修正箇所カウント追加

--- backend_regex/check_operators_space.py
+++ backend_regex/check_operators_space.py
@@ -5,12 +5,55 @@ REJEX_METHOD_NAME_BACK = "def([ |\t]+)(\\w+)([ |\t]*)\\((.*)\\)([ |\t]*)->([ |\t
 REJEX_CLASS_NAME = "class([ |\t]*)(\\w+)([ |\t]*)(\\((.*)\\))*([ |\t]*):"
 
 # 行ごとに実行 : 指定した演算子
+from method.naming import MethodNaming, ClassNaming
+from method.general import make_args
+from constants import REJEX_METHOD_NAME, REJEX_METHOD_NAME_BACK, REJEX_CLASS_NAME
 
 
-def check_operators_space(line: str):
+# 前後の空白を調整(1行分)
+def check_operators_space(line: str, method_naming: MethodNaming, class_naming: ClassNaming, mod_ope_num: int):
+    #print(f"met:{method_naming.method_lst}")
+    #print(class_naming.class_lst)
+    strip_str = line.strip()
+    # コメント行や空文字のみの行はpass
+    if strip_str.startswith('#') or strip_str == '':
+      return line, mod_ope_num
+
     if not (re.findall(REJEX_METHOD_NAME, line)
         or re.findall(REJEX_METHOD_NAME_BACK, line)
         or re.findall(REJEX_CLASS_NAME, line)):
+        #REJEX = (f'(\([^)]*\))')
+        #remove_str_line = re.sub(REJEX_STRING_SINGLE_STRICT + '|' + REJEX_STRING_DOUBLE_STRICT + '|' + REJEX_COMMENT, '', line)
+        
+        for s in list(set(method_naming.method_lst)):
+          REJEX = (f'{s}\s*\(.+\)')
+          if re.findall(REJEX, line):
+            args = make_args(re.findall(REJEX, line)[0])
+            strip_str = re.sub(f'{s}\s*\(.+\)', args, strip_str)
+
+        for s in list(set(class_naming.class_lst)):
+          REJEX = (f'{s}\s*\(.+\)')
+          if re.findall(REJEX, line):
+            args = make_args(re.findall(REJEX, line)[0])
+            strip_str = re.sub(f'{s}\s*\(.+\)', args, strip_str)
+
+        # 行のword内に' 'が2つ以上入っていたら' '1つにする
+        strip_str_lst = [s for s in re.split('\s', strip_str) if s != '']
+        #print(strip_str_lst)
+        n = len(strip_str_lst)
+        #print(n)
+        #REJEX = "([\w=]+( {2,}))" * n
+        # 行頭のインデントを取得
+        s = re.match(r" *", line).end() * ' '
+        for i, st in enumerate(strip_str_lst):
+          if i != len(strip_str_lst):
+            s += st + ' '
+          else:
+            s += st
+        line = s
+        #print("###########")
+        #print(line)
+
         # スライス内の演算子の前後にはスペースを追加しない
         if(not re.findall('\\[.*:.*\\]', line)):
             if(not re.findall('([a-zA-Z0-9]*)(<>|<=|>=|is not|not in|-=|==|\\+=|!=|=|\\+|-|\\*|/|%|<|>|and|or|not|in|is)([a-zA-Z0-9]*)',line)):
@@ -18,5 +61,7 @@ def check_operators_space(line: str):
                     '([a-zA-Z0-9]*)([\s]*)(<>|<=|>=|is not|not in|-=|==|\\+=|!=|=|\\+|-|\\*|/|%|<|>|and|or|not|in|is)([\s]*)([a-zA-Z0-9]*)',
                     '\\1 \\3 \\5',
                     line)
-    return line
+                mod_ope_num += 1 
+        # lineの末尾を確認
 
+    return line, mod_ope_num
